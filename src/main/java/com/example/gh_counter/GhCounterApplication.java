f6b5b2dcef06cc74b09e5d5931818c856e258d13
gvarma28 <gouravswork@gmail.com>
2025-05-24 00:39:51 +0530

    feat: initial changes

---
+++ src/main/java/com/example/gh_counter/GhCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.gh_counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gh_counter/GhCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gh_counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
